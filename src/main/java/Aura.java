68bfd6bd30370a09a9557c59d3d9cce985d0d711
Danny Chan <nihility.das@gmail.com>
2025-08-20 07:02:35 +0800

    feat: Implement echo

--- src/main/java/Aura.java
+++ src/main/java/Aura.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Aura {
     public static final String ASSISTANCE_NAME = "Aura";
     public static final String LOGO = "    _                   \n"
@@ -7,20 +9,38 @@ public class Aura {
                                     + "/_/   \\_\\__,_|_|  \\__,_|\n";
 
     public static void main(String[] args) {
-        printDividerLine();
-        System.out.println(Aura.LOGO);
-        System.out.println("Hello! I'm " + Aura.ASSISTANCE_NAME);
-        System.out.println("What can I do for you?");
-        printDividerLine();
-        System.out.println("Bye Bye. Hope to see you again soon!");
-        printDividerLine();
+        greeting();
+        echo();
+        exit();
     }
 
-    public static void printDividerLine() {
-        System.out.println("____________________________________________________________");
+    private static void formatReplyAndDisplay(String reply) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("        " + reply);
+        System.out.println("    ____________________________________________________________");
     }
 
-    public static void printLogo() {
-        System.out.println(Aura.LOGO);
+    private static void echo() {
+        String input;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                formatReplyAndDisplay("Bye Bye friend");
+                break;
+            }
+
+            formatReplyAndDisplay(input);
+        }
+    }
+
+    private static void greeting() {
+        formatReplyAndDisplay(Aura.LOGO
+                + "Hello! I'm " + Aura.ASSISTANCE_NAME + "\n"
+                + "What can I do for you?");
+    }
+
+    private static void exit() {
+        System.out.print("Bye Bye. Hope to see you again soon!");
     }
 }
