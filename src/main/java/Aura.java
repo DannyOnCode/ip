dc67e5daabbb18c086db8aa69f2ec898de9212a7
Danny Chan <nihility.das@gmail.com>
2025-08-20 09:27:04 +0800

    feat: Add exception handling for Mark and Unmark functionality

--- src/main/java/Aura.java
+++ src/main/java/Aura.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Aura {
     public static final String ASSISTANCE_NAME = "Aura";
     public static final String LOGO = "    _                   \n"
-                                    + "   / \\  _   _ _ __ __ _ \n"
-                                    + "  / _ \\| | | | '__/ _` |\n"
-                                    + " / ___ \\ |_| | | | (_| |\n"
-                                    + "/_/   \\_\\__,_|_|  \\__,_|\n";
+            + "   / \\  _   _ _ __ __ _ \n"
+            + "  / _ \\| | | | '__/ _` |\n"
+            + " / ___ \\ |_| | | | (_| |\n"
+            + "/_/   \\_\\__,_|_|  \\__,_|\n";
     private static final List<Task> lists = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -64,28 +64,48 @@ public class Aura {
     }
 
     private static void markTask(String input) {
+        try {
+            String trimmedIndex = input.substring(5).trim();
 
-        String trimmedIndex = input.substring(5).trim();
-
-        int index = Integer.parseInt(trimmedIndex);
-        Task task = lists.get(index - 1);
-        task.markAsDone();
-        printDivider();
-        replyPrint("Well Done sir! I've marked this task as done: ");
-        replyPrint(String.format("%s", task));
+            int index = Integer.parseInt(trimmedIndex);
+            Task task = lists.get(index - 1);
+            task.markAsDone();
+            printDivider();
+            replyPrint("Well Done sir! I've marked this task as done: ");
+            replyPrint(String.format("%s", task));
+        } catch (NumberFormatException e) {
+            printDivider();
+            System.out.println("You've used the command mark but the index is invalid.");
+            System.out.println("If this was intended, please use a different keyword (e.g \"check\")");
+        } catch (IndexOutOfBoundsException e) {
+            printDivider();
+            System.out.println("WHAT? Your input index was not in the list");
+        } finally {
+            printDivider();
+        }
     }
 
     private static void unMarkTask(String input) {
-        String trimmedIndex = input.substring(7).trim();
+        try {
+            String trimmedIndex = input.substring(7).trim();
 
-        int index = Integer.parseInt(trimmedIndex);
-        Task task = lists.get(index - 1);
-        task.markAsUnDone();
+            int index = Integer.parseInt(trimmedIndex);
+            Task task = lists.get(index - 1);
+            task.markAsUnDone();
 
-        printDivider();
-        replyPrint("Alright, I've marked this task as not done: ");
-        replyPrint(String.format("%s", task));
-
+            printDivider();
+            replyPrint("Alright, I've marked this task as not done: ");
+            replyPrint(String.format("%s", task));
+        } catch (NumberFormatException e) {
+            printDivider();
+            replyPrint("You've used the command unmark but the index is invalid.");
+            replyPrint("If this was intended, please use a different keyword (e.g \"uncheck\")");
+        } catch (IndexOutOfBoundsException e) {
+            printDivider();
+            replyPrint("WHAT? Your input index was not in the list");
+        } finally {
+            printDivider();
+        }
     }
 
     private static void printList() {
