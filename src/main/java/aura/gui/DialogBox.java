e849c91b6df08d33bc9637e0c02b07a37cf65c28
Danny Chan <nihility.das@gmail.com>
2025-09-04 13:50:37 +0800

    Modify Main.java and DialogBox formatting

---
+++ src/main/java/aura/gui/DialogBox.java
@@ -0,0 +1,33 @@
+package aura.gui;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * Custom component for Dialog
+ */
+public class DialogBox extends HBox {
+    private Label text;
+    private ImageView displayPicture;
+
+    /**
+     * Constructor for DialogBox
+     *
+     * @param s
+     * @param i
+     */
+    public DialogBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
